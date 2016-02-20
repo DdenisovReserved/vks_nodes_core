d883cb9381cc90a08d6fffc08f950bf0ee47ac4b
Igor Tomarov <adm6909@ya.ru>
2016-02-20 11:38:34 +0300

    merge new transport save mechanism

--- 456/js/attendance/support.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    var getContainerH = $(".fancybox-inner").attr("height");
-//    alert(getContainerH);
-    $(".fancybox-inner").css("overflow","hidden");
-    $(".tree-container").height($(window).height()-200);
-
-})
